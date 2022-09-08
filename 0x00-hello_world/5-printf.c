662d48319d1dfd0051b118eee75a9cc41bc63db1
SegoML <masegoletsoko8@gmail.com>
2022-09-08 10:41:37 -0700

    T5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: Using the main function
+ * program prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
