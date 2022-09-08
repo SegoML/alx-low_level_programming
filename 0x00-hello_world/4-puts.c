9382e04a2f9957742ddf14c4611c0077722dfadf
SegoML <masegoletsoko8@gmail.com>
2022-09-08 08:37:00 -0700

    Task 4.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * program prints prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
