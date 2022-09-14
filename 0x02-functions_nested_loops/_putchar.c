19e360935f782b39ae31bb90efc80852977d4af6
SegoML <masegoletsoko8@gmail.com>
2022-09-14 06:16:28 -0700

    T0.

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+/**
+ * main -print the string in the printf function
+ * Description : using the main function
+ * this program prints "_putchar"
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
